7e978d312bf2af174677479de7d0aec67e2c7b49
Alex Curran <alexcurran.va@gmail.com>
2025-05-28 17:11:31 -0500

    Add stylistic plugin to ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
+import stylistic from "@stylistic/eslint-plugin";
 import { defineConfig } from "eslint/config";
 
 
@@ -11,4 +12,19 @@ export default defineConfig([
     { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
     { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
     { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
+    {
+        plugins: {
+            "@stylistic": stylistic,
+        },
+        rules: {
+            "@stylistic/indent": ["error", 4],
+            "@stylistic/semi": ["error", "always"],
+            "@stylistic/quotes": ["error", "double"],
+            "@stylistic/brace-style": ["error", "1tbs"],
+            "@stylistic/arrow-parens": ["error", "always"],
+            "@stylistic/comma-dangle": ["error", "always-multiline"],
+            "@stylistic/comma-spacing": ["error", { "before": false, "after": true }],
+            "@stylistic/eol-last": ["error", "always"],
+        },
+    },
 ]);
