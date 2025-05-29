34c60cfbe53ae53d51254ac15fe138ee28d42bc3
Alex Curran <alexcurran.va@gmail.com>
2025-05-29 09:30:01 -0500

    Add general server error handler middleware and app listen func

--- server.js
+++ server.js
@@ -1,2 +1,33 @@
 "use strict";
 
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// API routers
+
+// general server error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack || err);
+
+    // check for nonsensical status codes by eliminating non ints
+    const statusCode = err.status && Number.isInteger(err.status) ? err.status : 500;
+    let errorDetails = {
+        error: {
+            message: err.message || "Server error",
+            stack: "",
+        },
+    };
+
+    if (process.env.DEV_DEBUG === "true") {
+        errorDetails.error.stack = err.stack;
+    }
+
+    res.status(statusCode).json(errorDetails);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}.`);
+});
