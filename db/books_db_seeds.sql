01cc2d6b6e3b23fc4b2892e3ecd625a59e1febb6
Alex Curran <alexcurran.va@gmail.com>
2025-05-30 12:20:18 -0500

    Add seeds SQL file for fake book and author data

---
+++ db/books_db_seeds.sql
@@ -0,0 +1,33 @@
+-- Seed data for the 'author' table
+INSERT INTO
+    author (author_id, first_name, last_name)
+VALUES
+    (1, 'J.R.R.', 'Tolkien'),
+    (2, 'Agatha', 'Christie'),
+    (3, 'Neil', 'Gaiman'),
+    (4, 'Mary', 'Shelley'),
+    (5, 'Carl', 'Sagan'),
+    (6, 'Octavia E.', 'Butler'),
+    (7, 'Jane', 'Austen'),
+    (8, 'Andy', 'Weir'),
+    (9, 'Stephen', 'King');
+
+-- Seed data for the 'book' table
+INSERT INTO
+    book (book_id, title, year, pages, genre, author_id)
+VALUES
+    (1, 'The Hobbit', 1937, 310, 'fantasy', 1),
+    (2, 'Murder on the Orient Express', 1934, 256, 'mystery', 2),
+    (3, 'American Gods', 2001, 480, 'fantasy', 3),
+    (4, 'Frankenstein; or, The Modern Prometheus', 1818, 280, 'horror', 4),
+    (5, 'Cosmos', 1980, 370, 'non-fiction', 5),
+    (6, 'Kindred', 1979, 288, 'sci-fi', 6),
+    (7, 'Pride and Prejudice', 1813, 350, 'romance', 7),
+    (8, 'Coraline', 2002, 170, 'horror', 3),
+    (9, 'Contact', 1985, 432, 'sci-fi', 5),
+    (10, 'And Then There Were None', 1939, 260, 'mystery', 2),
+    (11, 'Project Hail Mary', 2021, 496, 'sci-fi', 8),
+    (12, 'The Shining', 1977, 447, 'horror', 9),
+    (13, 'The Fellowship of the Ring', 1954, 423, 'fantasy', 1),
+    (14, 'Persuasion', 1817, 249, 'romance', 7),
+    (15, 'Parable of the Sower', 1993, 345, 'sci-fi', 6);
