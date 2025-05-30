382b5c161e807df4865b16cb186934616cc76487
Alex Curran <alexcurran.va@gmail.com>
2025-05-30 16:01:51 -0500

    Add getBooks controller logic

--- api/controllers/books-controller.js
+++ api/controllers/books-controller.js
@@ -5,7 +5,21 @@ const { db } = require("../../db/index");
 /** @type {import("express").RequestHandler} */
 function getBooks(req, res, next) {
     try {
-       
+        const statement = db.prepare(`
+            SELECT
+                book.book_id,
+                book.title,
+                book.year,
+                book.pages,
+                book.genre,
+                author.first_name,
+                author.last_name
+            FROM book INNER JOIN author
+                ON book.author_id = author.author_id;
+        `);
+        const books = statement.all();
+
+        res.status(200).json(books);
 
     } catch (err) {
         next(err);
