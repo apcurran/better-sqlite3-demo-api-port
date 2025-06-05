df2f17e29bfbef1fcbfa451b10a269800232cf28
Alex Curran <alexcurran.va@gmail.com>
2025-06-05 14:27:46 -0500

    Adjust db index file to create an in-memory SQLite3 db when NODE_ENV=test for running test suites for endpoints

--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
 "use strict";
 
 const path = require("path");
-
 const Database = require("better-sqlite3");
-const db = new Database(path.join(__dirname, "books.db"));
+
+const booksDbPath = path.join(__dirname, "books.db");
+const db = new Database(process.env.NODE_ENV === "test" ? ":memory:" : booksDbPath);
 db.pragma("journal_mode = WAL");
 
 module.exports = { db };
