2811a0de634ab5a2675934b3cecd088384f551ae
Alex Curran <alexcurran.va@gmail.com>
2025-06-10 11:23:42 -0500

    GET one specific book by id

--- api/tests/books.test.js
+++ api/tests/books.test.js
@@ -25,4 +25,22 @@ describe("books router tests", () => {
 
         assert.equal(response.body.length, 15);
     });
+
+    it("GET one specific book by id", async () => {
+        const bookId = 2;
+        const response = await request(app)
+            .get(`/api/books/${bookId}`)
+            .set("Accept", "application/json");
+        assert.match(response.headers["content-type"], /json/);
+        assert.equal(response.status, 200);
+
+        const { book_id, title, year, pages, genre, first_name, last_name } = response.body;
+        assert.equal(typeof book_id, "number");
+        assert.equal(typeof title, "string");
+        assert.equal(typeof year, "number");
+        assert.equal(typeof pages, "number");
+        assert.equal(typeof genre, "string");
+        assert.equal(typeof first_name, "string");
+        assert.equal(typeof last_name, "string");
+    });
 });
